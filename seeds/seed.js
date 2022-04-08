4b3e924f590513e612becf104ebc1b8d0d45fdd0
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-04-08 17:46:09 +0200

    bug fix

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const ProductInformation = require("../allProducts/allProducts"); // the DroneModel will be used to create new drones in our DB
-  
+
 const mongoose = require("mongoose");
 
 require("../db/index.js"); // this will execute the DB connection file
@@ -9,11 +9,12 @@ const ProductModel = require("../models/Product.model.js"); // this will require
 // .insertMany mongoose method to add an array of several elements to DB
 ProductModel.create(ProductInformation)
   .then((addedProduct) => {
-    addedProduct.forEach(eachProduct => console.log(eachProduct.Title)) // to console.log each added drone name
+    addedProduct.forEach((eachProduct) => console.log(eachProduct.title)); // to console.log each added drone name
     // to close the DB after data insertion
-    mongoose.connection.close()
-    .then(() => console.log("Database closed"))
-    .catch((err) => console.log("Error closing DB", err))
+    mongoose.connection
+      .close()
+      .then(() => console.log("Database closed"))
+      .catch((err) => console.log("Error closing DB", err));
   })
   .catch((err) => {
     console.log("Error with mongoose method", err);
