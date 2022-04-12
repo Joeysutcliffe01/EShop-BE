e4fc424d0d4307fefe835ea3f68a18d3c4739741
Ali Amidpour <ali.amidpour.eng@gmail.com>
2022-04-12 17:25:32 +0200

    before delete from cart page

---
+++ routes/cart.routes.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const Cart = require("../models/Cart.models");
+
+router.post("/cart", async (req, res) => {
+    console.log(req.body, "cartpage --------------");
+  
+    let cartItem = {
+      title: req.body.title,
+      imageUrl: req.body.imageUrl,
+      description: req.body.description,
+      price: req.body.price,
+      productId: req.body.productId,
+      owner: req.body.owner,
+    };
+  
+    let cartResponse = await Cart.create(cartItem);
+  
+    // res.status(400).json({
+    //     errorMessage: "Error in adding products from server! " + err.message,
+    //   });
+  
+    console.log(cartResponse);
+
+  });
+router.get("/allcart/:userid", async (req,res) =>{
+  let cart = await Cart.find();
+ 
+  let filterProduct = cart.filter((item) => {
+    return item.owner == req.params.userid
+  })
+  console.log(filterProduct, "filterproduct")
+  res.status(200).json({filterProduct})
+})
+
+
+router.delete("/delete/:_id", async (req, res) => {
+  try{
+      await Product.findByIdAndDelete(req.body.id);
+      return res.status(200).json({ success: true, msg: 'Product Deleted' });
+  }
+  catch(err){
+      console.error(err);
+  }
+});
+
+
+  module.exports = router;
