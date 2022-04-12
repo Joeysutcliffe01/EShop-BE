7afd5f475f0dc97d3cddfe4a1b4407bcd886e01e
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-04-12 18:21:07 +0200

    added delete to cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,46 +2,44 @@ const router = require("express").Router();
 const Cart = require("../models/Cart.models");
 
 router.post("/cart", async (req, res) => {
-    console.log(req.body, "cartpage --------------");
-  
-    let cartItem = {
-      title: req.body.title,
-      imageUrl: req.body.imageUrl,
-      description: req.body.description,
-      price: req.body.price,
-      productId: req.body.productId,
-      owner: req.body.owner,
-    };
-  
-    let cartResponse = await Cart.create(cartItem);
-  
-    // res.status(400).json({
-    //     errorMessage: "Error in adding products from server! " + err.message,
-    //   });
-  
-    console.log(cartResponse);
+  console.log(req.body, "cartpage --------------");
 
-  });
-router.get("/allcart/:userid", async (req,res) =>{
+  let cartItem = {
+    title: req.body.title,
+    imageUrl: req.body.imageUrl,
+    description: req.body.description,
+    price: req.body.price,
+    productId: req.body.productId,
+    owner: req.body.owner,
+  };
+
+  let cartResponse = await Cart.create(cartItem);
+
+  // res.status(400).json({
+  //     errorMessage: "Error in adding products from server! " + err.message,
+  //   });
+
+  console.log(cartResponse);
+});
+router.get("/allcart/:userid", async (req, res) => {
   let cart = await Cart.find();
- 
+
   let filterProduct = cart.filter((item) => {
-    return item.owner == req.params.userid
-  })
-  console.log(filterProduct, "filterproduct")
-  res.status(200).json({filterProduct})
-})
+    return item.owner == req.params.userid;
+  });
+  console.log(filterProduct, "filterproduct");
+  res.status(200).json({ filterProduct });
+});
 
-
-router.delete("/delete/:_id", async (req, res) => {
-  try{
-      await Product.findByIdAndDelete(req.body.id);
-      return res.status(200).json({ success: true, msg: 'Product Deleted' });
-  }
-  catch(err){
-      console.error(err);
+router.delete("/", async (req, res) => {
+  try {
+    console.log(req.body.id);
+    console.log(req.body);
+    await Cart.findByIdAndDelete(req.body.id);
+    return res.status(200).json({ success: true, msg: "Product Deleted" });
+  } catch (err) {
+    console.error(err);
   }
 });
 
-
-  module.exports = router;
+module.exports = router;
